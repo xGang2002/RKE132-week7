a199e6c368300fd32a87431c2ee0568e8f1a5b87
Sanya <sassik6969@gmail.com>
2023-03-22 18:27:00 +0200

    first commit

---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+} 
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
